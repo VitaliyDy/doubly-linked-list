785b8b8c29aa57877d497f57cfbb1a25c39a756e
Vitaliy Dybov <VitaliyDybov@gmail.com>
2017-09-17 10:53:27 +0300

    implement append(), head(), tail(), at()

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,15 +1,44 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = null;
+        this._tail = null;       
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        
+        if (this.length === 0){
+            this._head = this._tail = new Node(data, null, null);
+        } else {
+            let temp = this._tail;
+            this._tail = new Node(data, temp, null);
+            temp.next = this._tail;
+        } 
+        this.length ++;
+    }
 
-    head() {}
+    head() {
+        return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    at(index) {}
+    at(index) {
+        let current = this._head;        
+        if(index <= this.length){
+            for (let i = 0; i < index; i++) {   
+                current = current.next; 
+            }    
+            return current.data;
+        }   else {
+            return false;
+        }   
+                
+    }
 
     insertAt(index, data) {}
 
